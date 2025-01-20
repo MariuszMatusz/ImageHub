02924a6aee10c1c439e120a0d0cd40b5225ffd0e
MariuszMatusz <mariusz.matuszewskki@gmail.com>
2025-01-20 21:57:18 +0100

    First

---
+++ src/main/java/com/imagehub/imagehub/ImageHubApplication.java
@@ -0,0 +1,13 @@
+package com.imagehub.imagehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imagehub/imagehub/ImageHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagehub.imagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
